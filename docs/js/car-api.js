b9d77a352836b852f5bc4becbee856e8241c7850
Gaelll <gael.lelouarn@gmail.com>
2018-03-16 15:57:11 +0100

    modification car-api.js

--- docs/js/car-api.js
+++ docs/js/car-api.js
@@ -1,4 +1,4 @@
-const {getBrands} = require('node-car-api');
+/**const {getBrands} = require('node-car-api');
 const {getModels} = require('node-car-api');
 
 //const brands = getBrands();
@@ -9,16 +9,49 @@ const brands = getBrands().then(res => {
   console.log(res)
   }).catch(err=>{
   console.log(err)
-})
+});
 
 for(var i = 0; i < brands.length; i++){
 	const models = getModels(i).then(res => {
   console.log(res)
   }).catch(err=>{
   console.log(err)
-})
+});
+}*/
+
+
+const {getBrands} = require('node-car-api');
+const {getModels} = require('node-car-api');
+
+
+async function getModel(model)
+{
+  const models = await getModels(model);
+  return models;
 }
 
+async function getBrand()
+{
+  const brands = await getBrands();
+  return brands;
+}
+
+
+let brands = getBrand();
+
+brands.then(function(result)
+{
+  console.log(result);
+  for(var i = 0; i < result.length ; i++)
+  {
+    console.log(result[i]);
+    getModel(result[i]).then(res=>
+    {
+      console.log(res);
+    });
+  }
+
+});
 
 //
 //console.log(models);
