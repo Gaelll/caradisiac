e6180feae8bbc08a455031c3f17e15b276cb3add
Gaelll <gael.lelouarn@gmail.com>
2018-03-24 20:54:46 +0100

    Elastic Search connection

--- docs/js/car-api.js
+++ docs/js/car-api.js
@@ -2,29 +2,49 @@ const {getBrands} = require('node-car-api');
 const {getModels} = require('node-car-api');
 var express = require('express');
 var app = express();
+var elasticsearch = require('elasticsearch');
+
 
 app.listen(9292, function () {
   console.log("Listening on port 9292");
 });
 
-
-app.get('/populate', function (req, res) {
-  console.log("Hello");
-  let brands = getBrand();
-
-brands.then(function(result)
-{
-  console.log(result);
-  for(var i = 0; i < result.length ; i++)
-  {
-    console.log(result[i]);
-    getModel(result[i]).then(res=>
-    {
-      console.log(res);
-    });
-  }
-
+/*Creating client for Elastic Search*/
+var client = new elasticsearch.Client({
+  host: '192.168.99.100:9200',
+  log: 'trace'
 });
+
+
+app.get('/populate', function (req, resultat) {
+  let brands = getBrand();
+  brands.then(function(result)
+  {
+    client.indices.create({  
+      index: 'caradisiac'
+    },function(err,resp,status) {
+      if(err) {
+        console.log(err);
+      }
+      else {
+        console.log("create",resp);
+      }
+    });
+
+    for(var i = 0; i < result.length ; i++)
+    {
+      console.log(result[i]);
+      getModel(result[i]).then(res=>
+      {
+        console.log(res);
+      });
+    }
+    resultat.send("Indexation in ElasticSearch done !");
+  });
+});
+
+app.get('/suv', function (req, res) {
+
 })
 
 async function getModel(model)
